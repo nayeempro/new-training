7e511dce604ace5e8ef7da0507ea391a139a4e19
Md Nayeem <89269773+nayeempro@users.noreply.github.com>
2021-12-23 23:34:03 +0600

    Add files via upload

---
+++ cl11.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+/*
+type Book struct {
+	Title  string
+	Author string
+	Price  float32
+	Page   int
+}
+
+func main() {
+
+	var b1 Book
+	b1.Title = "hobe kisu ekta"
+	b1.Author = "Ami Nije"
+	b1.Price = 34
+	b1.Page = 3
+
+	fmt.Println(b1)
+	fmt.Println(b1.Title)
+	fmt.Println(b1.Author)
+	fmt.Println(b1.Page)
+
+}
+*/
+/*
+func main() {
+
+	b1 := struct {
+		Title  string
+		Author string
+		Price  float32
+		Page   int
+	}{
+		Title:  "hobe kisu ekta",
+		Author: "Ami Nije",
+		Price:  34,
+		Page:   3,
+	}
+
+	fmt.Println(b1)
+	fmt.Println(b1.Title)
+	fmt.Println(b1.Author)
+	fmt.Println(b1.Page)
+
+}
+*/
+
+func main() {
+
+	var w1 Weight
+	w1 = 30.32
+	fmt.Println(w1)
+}
